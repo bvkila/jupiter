4c850da51e403050e758e73784cb7987f04dd8cb
bvkila <joao.braga.pessoal@gmail.com>
2026-02-10 16:54:02 -0300

    fix: correção do setup

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup, find_packages
-
+ 
 setup(
-    name="automaweb", # Nome que será usado no 'pip install'
+    name="jupiter",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "selenium",
         # tkinter e os já vêm com o Python, não precisa listar
     ],
-    author="Joao Braga",
+    author="EOP/SUPCONC",
     description="Biblioteca para automaçao Web e gerenciamento de arquivos",
-    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     python_requires=">=3.6",
 )
